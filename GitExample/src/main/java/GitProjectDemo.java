0e23257b6bf4b61c3eaf28c3e51507cb48c833bb
aravitej <aravitej@DIN73003066.corp.capgemini.com>
2018-11-26 08:50:04 +0530

    Create method added

--- GitExample/src/main/java/GitProjectDemo.java
+++ GitExample/src/main/java/GitProjectDemo.java
@@ -11,6 +11,13 @@ public class GitProjectDemo {
 	public static void showMe() {
 		System.out.println("Another person can also edit my project");
 		System.out.println("Using Pull option");
+		System.out.println();
+		create();
+	}
+	public static void create() {
+		System.out.println("Welcome to Rj world : ");
+		System.out.println("________________________");
+		System.out.println("Im create method : ");
 	}
 
 }
